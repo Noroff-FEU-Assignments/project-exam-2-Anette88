6bddd46c849d8b7c298609b378dd71dae25dcbcf
Anette Gabrielsen <gabrielsen_anette@hotmail.com>
2021-11-14 19:34:25 +0100

    new try

--- holidaze/src/App.js
+++ holidaze/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
 import LoginPage from "./components/login/LoginPage";
 import Dashboard from "./components/dashboard/Dashboard";
+import Hotels from "./components/home/Hotels";
 import Nav from "./components/layout/Nav";
 import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
--- holidaze/src/constants/api.js
+++ holidaze/src/constants/api.js
@@ -1,2 +1,3 @@
+
 export const BASE_URL = "https://projectexam2.cutedevelop.no/wp-json/";
 export const TOKEN_PATH = "jwt-auth/v1/token";
